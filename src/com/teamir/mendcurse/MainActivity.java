90123e653b09251958a3c983e177da4e2821f688
Jin Xu <xu.jin.smile@gmail.com>
2013-07-23 23:08:13 +0800

    将应用改为横屏

--- src/com/teamir/mendcurse/MainActivity.java
+++ src/com/teamir/mendcurse/MainActivity.java
@@ -1,10 +1,12 @@
 package com.teamir.mendcurse;
 
-import com.agimind.widget.SlideHolder;
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.view.WindowManager;
+
+import com.agimind.widget.SlideHolder;
 
 public class MainActivity extends Activity {
 
@@ -13,6 +15,8 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
+		//去掉状态栏
+		this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		mSlideHolder = (SlideHolder) findViewById(R.id.slideHolder);
 		
 		
