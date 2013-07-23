2b16b19f2efe45d7b7a45b13d87f12ea6af0a454
Jin Xu <xu.jin.smile@gmail.com>
2013-07-23 19:21:55 +0800

    添加splashscreen，修改sidemenu

---
+++ src/com/teamir/mendcurse/Welcome.java
@@ -0,0 +1,25 @@
+package com.teamir.mendcurse;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+
+//欢迎界面
+public class Welcome extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);	
+		setContentView(R.layout.welcome);
+		//延迟1秒后执行run方法中的页面跳转
+		new Handler().postDelayed(new Runnable() {			
+			@Override
+			public void run() {
+				Intent intent = new Intent(Welcome.this, MainActivity.class);
+				startActivity(intent);
+				Welcome.this.finish();
+			}
+		}, 1000);
+	}
+}
